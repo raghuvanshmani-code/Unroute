9dfc2a6b92e168498d7c1dc4a68993cb5f203147
Raghuvansh <raghuvanshmani876@gmail.com>
2025-10-02 04:37:26 +0000

    when i try to go live on github pages, this appears? resolve this

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/Unroute',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +11,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
