e2bbb79d9d19bf8165ccb68b7296f3fc64b6a0e4
Raghuvansh <raghuvanshmani876@gmail.com>
2025-10-02 04:44:11 +0000

    commit changes

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+       {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   devIndicators: {
